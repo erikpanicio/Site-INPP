b361dbd6791f824a927632451f1f3c68574ab2dd
Erik Panicio <erikpanicio.dev@gmail.com>
2023-09-26 12:31:10 -0300

    Update script.js

--- Site-INPP/script.js
+++ Site-INPP/script.js
@@ -1,16 +1,16 @@
 function openMenu() {
-     document.body.style.overflow = "hidden"
-
     /* Animation */
     let menu_toggle = document.querySelector(".menu-toggle")
     menu_toggle.classList.toggle("active")
     
-    /* Open Menu */
+    /* Open and Close Menu */
     let menu_mobile = document.querySelector("#menu-mobile")
     if (menu_toggle.classList.contains("active") == true) {
         menu_mobile.style.display = "flex"
+        document.body.style.overflow = "hidden"
     }
     else {
         menu_mobile.style.display = "none"
+        document.body.style.overflow = "unset"
     }
 }
