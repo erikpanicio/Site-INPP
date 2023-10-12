1f312733fcfdbe4099e0871fcd1040d693ce2199
Erik Panicio <erikpanicio.dev@gmail.com>
2023-10-12 14:16:19 -0300

    arrumando detalhes

--- Site-INPP/script.js
+++ Site-INPP/script.js
@@ -3,7 +3,7 @@
 let menu_mobile = document.querySelector("#menu-mobile")
 let menu_toggle = document.querySelector(".menu-toggle")
 
-function openMenu() {
+function openMenu(){
     /* Animation */
     menu_toggle.classList.toggle("active")
     
@@ -30,7 +30,7 @@ for(let i = 0; i < link.length; i++){ /* Se algum botão for clicado fechar menu
 
 /* Slider Benefits */
 
-function transitionBenefits(num_benefits) {
+function transitionBenefits(num_benefits){
     /* Transition Slider */
     let slider2_benefits = document.querySelector("#benefits .slider2")
     let transition_porcent = num_benefits * 33.33
@@ -44,9 +44,9 @@ function transitionBenefits(num_benefits) {
     transition_button[num_benefits].style.backgroundColor = "#F205CB"
 }
 
-/* Slider Comments */
+/* Comments */
 
-function transitionComments(num_comments) {
+function transitionComments(num_comments){ /* Slider */
     /* Transition Slider */
     let slider2_comments = document.querySelector("#comments .slider2")
     let transition_porcent = num_comments * 33.33
@@ -60,8 +60,72 @@ function transitionComments(num_comments) {
     transition_button[num_comments].style.backgroundColor = "white"
 }
 
+/* Read More */
+
+/* let comment = document.querySelectorAll("#comments .comment")
+let comment_p = document.querySelectorAll("#comments p")
+for(let i = 0; i < comment_p.length; i++){
+    if(comment_p[i].clientHeight > 280){
+
+        let read_more = document.createElement("a");
+        read_more.innerHTML = "<strong>Leia Mais</strong>";
+        comment[i].appendChild(read_more)
+
+        let comment_a = document.querySelectorAll("#comments .comment a")
+        let count_comment_p = comment_p[i]
+
+        comment_p[i].classList.add("closed")
+
+        for(let i = 0; i < comment_a.length; i++){
+            comment_a[i].addEventListener("click", function(){
+                comment_p[i].classList.toggle("closed")
+                if(count_comment_p.classList.contains("closed") == true) {
+                    count_comment_p.style.height = "280px"
+                    count_comment_p.style.overflow = "hidden"
+                }
+                else if(count_comment_p.classList.contains("closed") == false) {
+                    count_comment_p.style.height = "auto"
+                    count_comment_p.style.overflow = "unset"
+                }
+            })
+        }
+
+    }
+} */
+
+let comment_p = document.querySelectorAll("#comments p");
+
+for (let i = 0; i < comment_p.length; i++) {
+    if (comment_p[i].clientHeight > 280) {
+
+        comment_p[i].style.height = "290px";
+        comment_p[i].style.overflow = "hidden";
+
+        let read_more = document.createElement("a");
+        read_more.innerHTML = "<strong>Ver mais</strong>";
+        
+        let comment = comment_p[i].closest(".comment");
+        comment.appendChild(read_more);
+
+        read_more.addEventListener("click", function () {
+            comment_p[i].classList.toggle("open");
+
+            if (comment_p[i].classList.contains("open")) {
+                read_more.innerHTML = "<strong>Ver menos</strong>";
+                comment_p[i].style.height = "auto";
+                comment_p[i].style.overflow = "unset";
+            } 
+            else {
+                read_more.innerHTML = "<strong>Ver mais</strong>";
+                comment_p[i].style.height = "290px";
+                comment_p[i].style.overflow = "hidden";
+            }
+        });
+    }
+}
+
 /* FAQ */
-function openAnswer(num_faq) {
+function openAnswer(num_faq){
     /* fundo e animação de girar */
     let box_question = document.querySelectorAll("#faq .box-question")
     box_question[num_faq].classList.toggle("active")
